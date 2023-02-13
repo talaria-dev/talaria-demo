eae1c4f92c104e97c1c2ebedd5fd9aaf3fb1cb6e
Zoran Krnetic <zoran.krnetic@gmail.com>
2023-02-13 09:42:34 +0100

    added support code functionality and demo page

--- demo/index.js
+++ demo/index.js
@@ -2,7 +2,7 @@ var url_protocol = window.location.protocol;
 var url_host = window.location.host;
 var rtc_server = url_protocol + "//" + url_host;
 
-console.log('rtc_server', rtc_server);
+console.log("rtc_server", rtc_server);
 
 var onlineUsers = null;
 
@@ -30,14 +30,17 @@ function infoTemplate(params) {
 
     if (params.name === "status_disconnected") {
       text =
-        "You are not connected. Please set the required fields and connect first.";
+        "You are not connected. Please set the required fields and connect.";
     }
 
     if (params.name === "users_empty") {
       text = "There are no online users in your room.";
     }
 
-    body = '<div class="empty">' + text + "</div>";
+    body =
+      '<div class="empty"><p>' +
+      text +
+      '</p><div class="quick-connect-btn">Connect</div></div>';
   }
 
   if (params.name === "status_connected") {
@@ -79,15 +82,17 @@ function infoTemplate(params) {
         '<li data-username="' +
         user.username +
         '">' +
-        '<img src="'+user.avatar+'" class="online-user-avatar" onerror="tlr.fixImage(this, \'person\')"/>' +
+        '<img src="' +
+        user.avatar +
+        '" class="online-user-avatar" onerror="tlr.fixImage(this, \'person\')"/>' +
         '<span class="name">' +
         user.full_name +
         "</span>" +
         '<div class="user-actions-box">' +
-          '<i class="material-icons action call-btn">call</i>' +
-          '<i class="material-icons action chat-btn">chat</i>' +
-          '<i class="material-icons action cobrowsing-btn">assistant</i>' +
-        '</div>' +
+        '<i class="material-icons action call-btn">call</i>' +
+        '<i class="material-icons action chat-btn">chat</i>' +
+        '<i class="material-icons action cobrowsing-btn">assistant</i>' +
+        "</div>" +
         "</li>";
     });
 
@@ -112,7 +117,7 @@ function infoTemplate(params) {
 
   if (params.name === "users_online") {
     $(".tlr-info li").on("click", ".action", function () {
-      var username = $(this).parent().parent().attr("data-username");      
+      var username = $(this).parent().parent().attr("data-username");
 
       if ($(this).hasClass("call-btn")) {
         tlr.performCall(username);
@@ -129,7 +134,14 @@ function infoTemplate(params) {
     $(".tlr-info .update-presence-btn").on("click", function () {
       tlr.updatePresenceUi();
     });
-  }  
+  }
+
+  if (params.name === "status_disconnected") {
+    $(".tlr-info .quick-connect-btn").on("click", function () {
+      $(".tlr-info").remove();
+      _connect();
+    });
+  }
 
   $(".tlr-info .close-btn").on("click", function () {
     $(this).parent().parent().remove();
@@ -180,7 +192,6 @@ function openInfo(params) {
 
 function generateToken() {}
 
-
 function updateOnlineStatus(onOff) {
   var $text = $online_status.find(".text");
   var status = onOff ? "Connected" : "Disconnected";
@@ -213,7 +224,7 @@ $(".card-title-box").on("click", function () {
   $(this).parent().toggleClass("info-expanded");
 });
 
-$("#connect").on("click", function () {
+var _connect = function () {
   if (easyrtc.myEasyrtcid) {
     tlr.disconnect();
   } else {
@@ -223,27 +234,31 @@ $("#connect").on("click", function () {
       return false;
     }
 
-    tlr.connect().then(function(){
-      saveCredentialsInLS();
-    
-    }).catch(function (error) {      
-      updateOnlineStatus(false);
+    tlr
+      .connect()
+      .then(function () {
+        saveCredentialsInLS();
+      })
+      .catch(function (error) {
+        updateOnlineStatus(false);
 
-      console.log('### CONNECT : ERROR :', error);
+        console.log("### CONNECT : ERROR :", error);
 
-      tlr.notify({
-        icon: "warning",
-        icon_color: "red",
-        message:
-          "Error connecting to the rtc server. Error code: " +
-          error.code +
-          ". Error message: " +
-          error.message,
-        autohide: true,
+        tlr.notify({
+          icon: "warning",
+          icon_color: "red",
+          message:
+            "Error connecting to the rtc server. Error code: " +
+            error.code +
+            ". Error message: " +
+            error.message,
+          autohide: true,
+        });
       });
-    });
   }
-});
+};
+
+$("#connect").on("click", _connect);
 
 $("#apply_changes").on("click", function () {
   if (tlr.callInProgress()) {
@@ -295,7 +310,7 @@ $("#generate_token").on("click", function () {
   $("#client_token_1").toggleClass("error", false);
 
   var client_id = $("#client_id_1").val();
-  var client_token = $("#client_token_1").val();  
+  var client_token = $("#client_token_1").val();
 
   if (client_id === "" || client_token === "") {
     if (client_id === "") {
@@ -365,47 +380,95 @@ $("#quick_call").on("click", function () {
     });
 
     return;
-  } 
-  
-  var invited_person_name = $("#invited_person_name").val();  
+  }
+
+  var invited_person_name = $("#invited_person_name").val();
 
   if (invited_person_name === "") {
     invited_person_name = "Test " + Math.floor(100000 + Math.random() * 900000);
     $("#invited_person_name").val(invited_person_name);
   }
-  
-  tlr.quickLinks.createLink(invited_person_name).then(function(data){    
-    var localtime = new Date(data.expiration_time).toString().split(" GMT")[0];
 
-    var link_html =
-      "" +
-      '<div class="quick-link-box">' +
-      '<div class="label">Quick call link for ' +
-      invited_person_name +
-      ",</div>" +
-      '<div class="expiration">expires at ' +
-      localtime +
-      ".</div>" +
-      '<a class="link" href="' +
-      data.quick_call_link +
-      '" target="_blank">' +
-      data.quick_call_link +
-      "</a>" +
-      "</div>";
+  tlr.quickLinks
+    .createLink(invited_person_name)
+    .then(function (data) {
+      var localtime = new Date(data.expiration_time)
+        .toString()
+        .split(" GMT")[0];
 
-    copyTextToClipboard(data.quick_call_link);
+      var link_html =
+        "" +
+        '<div class="quick-link-box">' +
+        '<div class="label">Quick call link for ' +
+        invited_person_name +
+        ",</div>" +
+        '<div class="expiration">expires at ' +
+        localtime +
+        ".</div>" +
+        '<a class="link" href="' +
+        data.quick_call_link +
+        '" target="_blank">' +
+        data.quick_call_link +
+        "</a>" +
+        "</div>";
 
-    $("#quick_call_links").append(link_html);
+      copyTextToClipboard(data.quick_call_link);
 
-    $("#invited_person_name").val("");
-  }).catch(function(error){
+      $("#quick_call_links").append(link_html);
+
+      $("#invited_person_name").val("");
+    })
+    .catch(function (error) {
+      tlr.notify({
+        icon: "warning",
+        icon_color: "red",
+        message: "Error: " + error,
+        autohide: true,
+      });
+    });
+});
+
+$("#support_code_btn").on("click", function () {
+  if (!tlr.connected) {
     tlr.notify({
       icon: "warning",
       icon_color: "red",
-      message:"Error: " + error,        
+      message: "You are not connected. Please connect and try again.",
       autohide: true,
     });
-  });  
+
+    return;
+  }
+
+  tlr.supportCode
+    .createSupportCode()
+    .then(function (data) {
+      var localtime = new Date(data.expiration_time)
+        .toString()
+        .split(" GMT")[0];
+
+      var code_html =
+        "" +
+        '<div class="quick-link-box">' +
+        '<div class="label">Support code ' +
+        data.quick_call_token +
+        ",</div>" +
+        '<div class="expiration">expires at ' +
+        localtime +
+        ".</div>" ;
+
+      copyTextToClipboard(data.quick_call_token);
+
+      $("#create_support_code").append(code_html);
+    })
+    .catch(function (error) {
+      tlr.notify({
+        icon: "warning",
+        icon_color: "red",
+        message: "Error: " + error,
+        autohide: true,
+      });
+    });
 });
 
 $("#open_quick_links_list").on("click", function () {
@@ -441,7 +504,7 @@ $("#media_message_btn").on("click", function () {
   }
 
   tlr.createMediaMessage({
-    recipient_name: recipient_name
+    recipient_name: recipient_name,
   });
 
   $("#recipient_name").val("");
@@ -468,7 +531,7 @@ $("#screencast_btn").on("click", function () {
 
   tlr.createMediaMessage({
     screencast: true,
-    title: title
+    title: title,
   });
 
   $("#screencast_title").val("");
@@ -516,85 +579,13 @@ $(".link-btn").on("click", function () {
   }
 });
 
-$("#confroom_btn").on("click", function () {
-  if (tlr.connected) {
-    if (tlr.config.credentials.client_id === "syldemo") {
-      tlr.notify({
-        icon: "warning",
-        icon_color: "red",
-        message:
-          "You can't create conference room with the demo account. Contact us in order to get a valid account.",
-        autohide: true,
-      });
-    } else {
-      tlr.confRoom.open();
-    }
-  } else {
-    tlr.notify({
-      icon: "warning",
-      icon_color: "red",
-      message:
-        "You have to be connected in order to create the conference room",
-      autohide: true,
-    });
-  }
-});
+$("#confroom_btn").on("click", tlr.confRoom.open);
 
-$("#open_confrooms_list").on("click", function () {
-  if (!tlr.connected) {
-    tlr.notify({
-      icon: "warning",
-      icon_color: "red",
-      message:
-        "You need to be connected in order to view list of conference rooms.",
-      autohide: true,
-    });
-  } else {
-    tlr.confRoom.openList();
-  }
-});
+$("#open_confrooms_list").on("click", tlr.confRoom.openList);
 
-$("#call_phone_number_btn").on("click", function () {
-  if (tlr.config.credentials.client_id === "syldemo") {
-    tlr.notify({
-      icon: "warning",
-      icon_color: "red",
-      message:
-        "You can't call phone numbers with the demo account. Contact us in order to get a valid account.",
-      autohide: true,
-    });
-  } else if (!tlr.connected) {
-    tlr.notify({
-      icon: "warning",
-      icon_color: "red",
-      message: "You need to be connected in order to call phone.",
-      autohide: true,
-    });
-  } else {
-    tlr.phoneCall.open();
-  }
-});
+$("#call_phone_number_btn").on("click", tlr.phoneCall.open);
 
-$("#send_sms_btn").on("click", function () {
-  if (tlr.config.credentials.client_id === "syldemo") {
-    tlr.notify({
-      icon: "warning",
-      icon_color: "red",
-      message:
-        "You can't send sms messages with the demo account. Contact us in order to get a valid account.",
-      autohide: true,
-    });
-  } else if (!tlr.connected) {
-    tlr.notify({
-      icon: "warning",
-      icon_color: "red",
-      message: "You need to be connected in order to send sms message.",
-      autohide: true,
-    });
-  } else {
-    tlr.sms.open();
-  }
-});
+$("#send_sms_btn").on("click", tlr.sms.open);
 
 $("form input").on("change", function () {
   $(this).toggleClass("error", false);
@@ -610,7 +601,7 @@ $online_users_btn.on("click", function () {
   openInfo({ name: "users_" + online });
 });
 
-$("#open_popup").on("click", function(e){
+$("#open_popup").on("click", function (e) {
   if (!tlr.connected) {
     tlr.notify({
       icon: "warning",
@@ -623,45 +614,33 @@ $("#open_popup").on("click", function(e){
   }
 });
 
-$("#open_confrooms_recordings").on("click", function () {
-  if (!tlr.connected) {
-    tlr.notify({
-      icon: "warning",
-      icon_color: "red",
-      message:
-        "You need to be connected in order to view list of conference rooms recordings.",
-      autohide: true,
-    });
-  } else {
-    tlr.confRoom.openRecordings();
-  }
-});
-
+$("#open_confrooms_recordings").on("click", tlr.confRoom.openRecordings);
 
 // ----------------------------------------------
 // TALARIA
 // ----------------------------------------------
 
-function getTalariaToken(){
-  console.log('# getTalariaToken : START');
+function getTalariaToken() {
+  console.log("# getTalariaToken : START");
   return new Promise((resolve, reject) => {
     $.post(
       "/api/auth/",
       {
-        client_id: $('#client_id').val(),        
-        client_token: $('#client_token').val(),
+        client_id: $("#client_id").val(),
+        client_token: $("#client_token").val(),
       },
       function (data, status, xhr) {
         var res_obj = JSON.parse(data);
-        console.log('# getTalariaToken : END : ', res_obj);        
+        console.log("# getTalariaToken : END : ", res_obj);
 
         if (!res_obj.success) {
           reject(res_obj.error);
-        } else {          
+        } else {
           resolve(res_obj.data.auth_token);
         }
-      }, function(err){
-        console.log('# getTalariaToken : ERROR :', err);
+      },
+      function (err) {
+        console.log("# getTalariaToken : ERROR :", err);
       }
     );
   });
@@ -711,7 +690,7 @@ function config() {
     show_buttons_label = false;
   } else {
     show_buttons_label = false;
-  }  
+  }
 
   var language = $("#language").val() || "en";
   var ui_size = $("#ui_size").val();
@@ -722,19 +701,17 @@ function config() {
   var user_email = $("#user_email").val();
   var client_id = $("#client_id").val();
   var client_token = $("#client_token").val();
-  var auth_token = null;  
+  var auth_token = null;
 
   var credentials = {
-    client_id: client_id
-  };  
+    client_id: client_id,
+  };
 
-  if ($("#auth_token").val() !== '') {
+  if ($("#auth_token").val() !== "") {
     credentials.auth_token = $("#auth_token").val();
   } else {
     credentials.client_token = client_token;
   }
-  
-  console.log(credentials);
 
   var has_errors = false;
 
@@ -761,7 +738,7 @@ function config() {
 
   if (has_errors) {
     return false;
-  }  
+  }
 
   tlr.configure({
     room_name: room_name,
@@ -779,53 +756,67 @@ function config() {
       id: user_id,
       full_name: full_name,
     },
-    avatar_path: 'https://i.pravatar.cc/150?img={id}',
+    avatar_path: "https://i.pravatar.cc/150?img={id}",
     credentials: credentials,
     //using i18n keys you can override any UI copy from tlr-client/app/langs/en.js language file
     i18n: {
       processing_message: "Processing your message, please wait...",
-      message_uploaded: "Your message is ready, here is the message link that you can send to the message recipient.",
+      message_uploaded:
+        "Your message is ready, here is the message link that you can send to the message recipient.",
     },
+    contacts: [
+      {
+        id: "0001",
+        full_name: "Justin Shaw",
+      },
+      {
+        id: "0002",
+        full_name: "Oscar Walker",
+      },
+      {
+        id: "0003",
+        full_name: "William McKnight",
+      },
+      {
+        id: "0004",
+        full_name: "Joseph Watson",
+      },
+      {
+        id: "0005",
+        full_name: "Anthony Harris",
+      },
+      {
+        id: "0006",
+        full_name: "Martha Taylor",
+      },
+      {
+        id: "0007",
+        full_name: "Krista Martin",
+      },
+      {
+        id: "0008",
+        full_name: "Carolyn Goldberg",
+      },
+      {
+        id: "0009",
+        full_name: "Tracie Amon",
+      },
+      {
+        id: "0010",
+        full_name: "Elicia White",
+      },
+    ],
+    getFreshAuthToken: getTalariaToken,
     // You will need that if you want to
     // to save chat messages on your server
     // chat_message_getter: getChatMessages,
-    contacts: [
-      {
-        id: '0001',
-        full_name: 'Justin Shaw'
-      }, {
-        id: '0002',
-        full_name: 'Oscar Walker'
-      }, {
-        id: '0003',
-        full_name: 'William McKnight'
-      }, {
-        id: '0004',
-        full_name: 'Joseph Watson'
-      }, {
-        id: '0005',
-        full_name: 'Anthony Harris'
-      }, {
-        id: '0006',
-        full_name: 'Martha Taylor'
-      }, {
-        id: '0007',
-        full_name: 'Krista Martin'
-      }, {
-        id: '0008',
-        full_name: 'Carolyn Goldberg'
-      }, {
-        id: '0009',
-        full_name: 'Tracie Amon'
-      }, {
-        id: '0010',
-        full_name: 'Elicia White'
-      }
-    ],
-    getFreshAuthToken: getTalariaToken
+    // chat_message_setter: saveChatMessage
   });
 
-  if (tlr.config.credentials.hasOwnProperty('auth_token') && tlr.config.credentials.hasOwnProperty('client_token')) {
+  if (
+    tlr.config.credentials.hasOwnProperty("auth_token") &&
+    tlr.config.credentials.hasOwnProperty("client_token")
+  ) {
     delete tlr.config.credentials.client_token;
   }
 
@@ -854,21 +845,27 @@ tlr.on("disconnected", function (_onlineUsers) {
   updateOnlineStatus(false);
 });
 
+tlr.on("confroomjoined", function (roomInfo) {
+  console.log("# Conference room joined :", roomInfo);
+});
+
+tlr.on("confroomleft", function (roomInfo) {
+  console.log("# Conference room left :", roomInfo);
+});
+
 /*
 tlr.on("popupclosed", function(){
   // this will prevent reconnection in main window
   tlr.popup.preventReconnect();
 });
 */
- 
-/*
-// Example of saving chat messages on your server
 
-tlr.on("chatmessage", function (message) {
+function saveChatMessage(message) {
+  console.log("### saveChatMessage :", message);
   var _message = {
     participants: message.participants.join(),
     created_at: message.created_at,
-    created_by: message.created_by
+    created_by: message.created_by,
   };
 
   if (message.file) {
@@ -881,28 +878,32 @@ tlr.on("chatmessage", function (message) {
 
   let data = {
     message: _message,
-    client_id: tlr.config.credentials.client_id    
+    client_id: tlr.config.credentials.client_id,
   };
 
   if ($("#auth_token").val()) {
     data.auth_token = $("#auth_token").val();
   } else {
     data.client_token = $("#client_token").val();
-  }    
+  }
 
-  $.ajax({
-    type: "POST",
-    url: "/api/save_chat_message/",
-    contentType: "application/json; charset=utf-8",
-    data: JSON.stringify(data),
-    success: function (res) {},
+  return new Promise(function (resolve, reject) {
+    $.ajax({
+      type: "POST",
+      url: "/api/save_chat_message/",
+      contentType: "application/json; charset=utf-8",
+      data: JSON.stringify(data),
+      success: function (res) {
+        resolve();
+      },
+    });
   });
-});
+}
 
 function getChatMessages(participants) {
   return new Promise(function (resolve, reject) {
     var data = {
-      client_id: $("#client_id").val(),      
+      client_id: $("#client_id").val(),
       participants: participants.join(),
     };
 
@@ -910,7 +911,7 @@ function getChatMessages(participants) {
       data.auth_token = $("#auth_token").val();
     } else {
       data.client_token = $("#client_token").val();
-    }    
+    }
 
     $.get("/api/get_chat_conversation/", data, function (data) {
       var messages = data.data.map(function (msg) {
@@ -924,7 +925,6 @@ function getChatMessages(participants) {
     });
   });
 }
-*/
 
 tlr.init();
 
